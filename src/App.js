9bf5c137f2bb13f09c8e72b92d93a50a9665518e
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-11-12 20:26:43 -0300

    Finished exercise one at the first try \o/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+    }
+    this.handleButton = this.handleButton.bind(this);
+  }
+
+  handleButton(car) {
+    this.setState((prevState) => ({
+      [car]: !prevState[car],
+    }));
+  }
+  
+  render() {
+    const contextValue = {
+      ...this.state,
+      handle: this.handleButton,
+    }
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
